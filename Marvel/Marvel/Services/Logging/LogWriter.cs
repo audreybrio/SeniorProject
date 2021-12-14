16948d6fcb64d6deeff0250063ff0c48e871bc5f
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2021-12-14 13:33:44 -0800

    Added DbLogWriter class

--- Marvel/Marvel/Services/Logging/LogWriter.cs
+++ Marvel/Marvel/Services/Logging/LogWriter.cs
@@ -65,6 +65,88 @@ namespace Marvel.Services.Logging
         }
     }
 
+    // Writes logs to a database. The connection string must be configured as an environment variable,
+    // or set after instantiation, otherwise this will fail.
+    public class DbLogWriter : LogWriter
+    {
+        private Queue<Log> logs { get; }
+        public string dbConnectionString { get; set; } = "";
+        public string tableName { get; set; } = "logs";
+        public DbLogWriter()
+        {
+            try
+            {
+                dbConnectionString = Environment.GetEnvironmentVariable("MARVELCONNECTIONSTRING");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The following exception has occurred: " +
+                                        ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+            }
+            logs = new Queue<Log>();
+        }
+        // Add a new log to the queue. Returns the number of logs successfully added.
+        // Intended to be called with a discard, like so: _ = myLogWriter.AddLog(...);
+        // Reference: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
+        public override async Task<int> AddLog(string category, string level, string user, string description)
+        {
+            int result = 0;
+            Log newLog = new Log(category, level, user, description);
+            try
+            {
+                logs.Enqueue(newLog);
+                result = await this.WriteAllLogs();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The following exception has occurred: " +
+                                    ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+        public override async Task<int> WriteAllLogs()
+        {
+            if (logs.Count < 1)
+            {
+                return 0;
+            }
+            int result = 0;
+            while (logs.Count > 0)
+            {
+                int rowsAffected = 0;
+                try
+                {
+                    Log current = logs.Dequeue();
+                    using (SqlConnection connection = new SqlConnection(dbConnectionString))
+                    {
+                        string query = "INSERT INTO " + this.tableName + " (timestamp, category, layer, username, message) "
+                                                 + "VALUES (@timestamp, @category, @layer, @username, @message);";
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@timestamp", current.timestamp);
+                            command.Parameters.AddWithValue("@category", current.category);
+                            command.Parameters.AddWithValue("@layer", current.level);
+                            command.Parameters.AddWithValue("@username", current.user);
+                            command.Parameters.AddWithValue("@message", current.message);
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The following exception has occurred: " +
+                                        ex.GetType().FullName);
+                    Console.WriteLine(ex.Message);
+                }
+                result += rowsAffected;
+            }
+            return result;
+        }
+    }
+
     public class FileLogWriter : LogWriter
     {
         private Queue<Log> logs { get; }
