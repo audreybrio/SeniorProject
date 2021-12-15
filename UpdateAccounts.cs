305afbfddff6eae07d84ffc7b494630883d3838b
Albert Toscano <57019251+tj5775@users.noreply.github.com>
2021-12-15 10:50:38 -0800

    Add files via upload

--- UpdateAccounts.cs
+++ UpdateAccounts.cs
@@ -4,81 +4,102 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Data.SqlClient;
 
-namespace Rextester{
-    public class Program{
-        
+namespace Rextester
+{
+    public class Program
+    {
+
         public static string getUserRole(string username)
         {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = Environment.GetEnvironmentVariable("MARVELCONNECTIONSTRING");
             conn.Open();
-            SqlCommand cmd = new SqlCommand("SELECT role" + " from UserTable " + "WHERE UserTable.role = role", conn);
+            SqlCommand cmd = new SqlCommand("SELECT role" + " from UserTable " + "WHERE UserTable.username = @username AND UserTable.role = role", conn);
+            cmd.Parameters.AddWithValue("@username", username);
             cmd.ExecuteNonQuery();
             string role = "";
             role = Convert.ToString(cmd.ExecuteScalar());
             return role;
         }
 
-        public static bool userExist(string username){
+        public static bool userExist(string username)
+        {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = Environment.GetEnvironmentVariable("MARVELCONNECTIONSTRING");
             conn.Open();
             SqlCommand cmd = new SqlCommand("SELECT COUNT(username)" + " from UserTable" + " WHERE UserTable.username = @username", conn);
-            cmd.Parameters.AddWithValue("@username", userName);
+            cmd.Parameters.AddWithValue("@username", username);
             cmd.ExecuteNonQuery();
             int count = (int)cmd.ExecuteScalar();
-            if(count > 0){
+            if (count > 0)
+            {
                 return true;
             }
-            else{
+            else
+            {
                 return false;
             }
         }
 
-        public static void updateUserRole(string username, string newRole){
+        public static void updateUserRole(string username, string newRole)
+        {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = Environment.GetEnvironmentVariable("MARVELCONNECTIONSTRING");
             conn.Open();
             SqlCommand cmd = new SqlCommand("UPDATE UserTable" + " SET role = @newRole" + " WHERE username = @username", conn);
             cmd.Parameters.AddWithValue("@newRole", newRole);
-            cmd.Parameters.AddWithValue("@username", userName);
+            cmd.Parameters.AddWithValue("@username", username);
             cmd.ExecuteNonQuery();
         }
-        
+
         // Main 
         public static void Main(string[] args)
         {
-            string CurrentUsername = "abrio"
-            if(getUserRole(CurrentUsername) == "Admin"){
+            string CurrentUsername = "abrio";
+            if (getUserRole(CurrentUsername) == "admin")
+            {
                 SqlConnection conn = new SqlConnection();
                 conn.ConnectionString = Environment.GetEnvironmentVariable("MARVELCONNECTIONSTRING");
                 conn.Open();
-                Console.WriteLine("Enter username to update account: ")
+                Console.WriteLine("Enter username to update account: ");
                 string userSelected = Console.ReadLine();
-                if(userExist(userSelected) == true){
-                    if(getUserRole(userSelected) == "Admin"){
-                        Console.WriteLine("Do you want to change " + userSelected + "' role to Student? Y/N")
+                if (userExist(userSelected) == true)
+                {
+                    Console.WriteLine(userSelected);
+                    Console.WriteLine(getUserRole(userSelected));
+                    if (getUserRole(userSelected) == "admin")
+                    {
+                        Console.WriteLine("Do you want to change " + userSelected + "' role to student? Y/N");
                         string response = Console.ReadLine();
-                        if(response == "Y"){
-                            updateUserRole(userSelected, "Student")
+                        if (response == "Y"|| response == "y")
+                        {
+                            updateUserRole(userSelected, "student");
                         }
-                        else{
-                            Console.WriteLine(userSelected + " will NOT be changed to Student")
+                        else
+                        {
+                            Console.WriteLine(userSelected + " will NOT be changed to Student");
                         }
                     }
-                    if(getUserRole(userSelected) == "Student"){
-                        Console.WriteLine("Do you want to change " + userSelected + "' role to Admin? Y/N")
+                    else if (getUserRole(userSelected) == "student")
+                    {
+                        Console.WriteLine("Do you want to change " + userSelected + "' role to admin? Y/N");
                         string response = Console.ReadLine();
-                        if(response == "Y"){
-                            updateUserRole(userSelected, "Admin")
+                        if (response == "Y" || response == "y")
+                        {
+                            updateUserRole(userSelected, "admin");
                         }
-                        else{
-                            Console.WriteLine(userSelected + " will NOT be changed to Admin")
+                        else
+                        {
+                            Console.WriteLine(userSelected + " will NOT be changed to admin");
                         }
                     }
                 }
             }
-               
+            else
+            {
+                Console.WriteLine("Not Authorized");
+            }
+
         }
     }
 }
