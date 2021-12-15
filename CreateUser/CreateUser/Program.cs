f1d816cee56460c4dda3e75de66fec2a2115e0a9
jlcutri <61342277+jlcutri@users.noreply.github.com>
2021-12-15 02:09:58 -0800

    Create User

---
+++ CreateUser/CreateUser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
