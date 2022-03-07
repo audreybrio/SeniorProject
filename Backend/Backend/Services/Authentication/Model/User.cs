b5166879b74f710be42cc21bc5e68b7bcebc08a7
audreybrio <49995869+audreybrio@users.noreply.github.com>
2022-03-06 20:31:48 -0800

    User

--- Backend/Backend/Services/Authentication/Model/User.cs
+++ Backend/Backend/Services/Authentication/Model/User.cs
@@ -7,17 +7,36 @@ namespace StudentMultiTool.Backend.Services.Authentication.Model
 {
     public class User
     {
-        [Required(ErrorMessage = "This Field is Required")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "This Field is Required")]
-        public string Passcode { get; set; }
 
-        [Required(ErrorMessage = "This Field is Required")]
-        public string Username { get; set; }
+        public string name;
+        public string username;
+        public string email;
+        public string passcode;
+        public string role;
+        public string school;
+        public bool active;
 
-        [Required(ErrorMessage = "This Field is Required")]
-        [DataType(DataType.Password)]
-        public string OTP { get; set; }
+
+        public User(string n, string u, string e, string pass, string r, string s, bool a)
+        {
+            this.name = n;
+            this.username = u;
+            this.email = e;
+            this.passcode = pass;
+            this.role = r;
+            this.school = s;
+            this.active = a;
+
+
+        }
+        public string Name { get => name; set => name = value; }
+        public string Username { get => username; set => username = value; }
+        public string Role { get => role; set => role = value; }
+        public bool Active { get => active; set => active = value; }
+        public string Email { get => email; set => email = value; }
+        public string Passcode { get => passcode; set => passcode = value; }
+
+        public string School { get => school; set => school = value; }
 
         public string LoginErrorMsg { get; set; }
         public string Login2ErrorMsg { get; set; }
