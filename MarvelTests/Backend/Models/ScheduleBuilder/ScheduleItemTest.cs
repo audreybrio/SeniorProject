13a586d131f4f685d4e6c46d2a1b14566ca9ef79
aloafofbrad <aloafofbrad2@gmail.com>
2022-03-06 23:23:22 -0800

    Relocated

--- MarvelTests/Backend/Models/ScheduleBuilder/ScheduleItemTest.cs
+++ MarvelTests/Backend/Models/ScheduleBuilder/ScheduleItemTest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using StudentMultiTool.Backend.Models;
+using StudentMultiTool.Backend.Models.ScheduleBuilder;
 
 namespace Tests
 {
