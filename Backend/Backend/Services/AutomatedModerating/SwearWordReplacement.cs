0941e58c12972090c7c5073d0a358378f0900439
jlcutri <61342277+jlcutri@users.noreply.github.com>
2022-03-07 17:25:16 -0800

    initial commit

---
+++ Backend/Backend/Services/AutomatedModerating/SwearWordReplacement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Automated_Moderating_demo
+{
+    public sealed class SwearWordReplacement
+    {
+        public SwearWordReplacement(IEnumerable<KeyValuePair<string, string>> replacements)
+        {
+            Replacements = replacements.Select(p => new ProfanityFilter(p.Key, p.Value)).ToList().AsReadOnly();
+        }
+
+        public IReadOnlyList<ProfanityFilter> Replacements { get; }
+
+        public string Clbuttify(string message)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                foreach (var replacement in Replacements)
+                {
+                    message = replacement.Replace(message);
+                }
+            }
+
+            return message;
+        }
+    }
+}
