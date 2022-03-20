bf8f6c56568e09d97f4a1100309d5f09869a960f
audreybrio <49995869+audreybrio@users.noreply.github.com>
2022-03-19 17:15:24 -0700

    ridding logout

--- StudentMultiTool/Backend/Services/Authentication/Controller/LoginController.cs
+++ StudentMultiTool/Backend/Services/Authentication/Controller/LoginController.cs
@@ -1,4 +1,4 @@
-﻿using StudentMultiTool.Backend.Services.Logout;
+﻿using StudentMultiTool.Backend.Services.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
 using System.Data.SqlClient;
@@ -64,8 +64,7 @@ namespace StudentMultiTool.Backend.Services.Authentication.Controller
                         {
                             Console.Write("Login Success");
                             // Changed out for redirct to homepage
-                            LogoutController logout = new LogoutController();
-                            logout.Logout();
+                            Logout();
 
                             break;
                         }
@@ -312,7 +311,42 @@ namespace StudentMultiTool.Backend.Services.Authentication.Controller
 
         public void Logout()
         {
+            Console.WriteLine("Welcome to Student Multi-Tool Home Page");
+            Console.Title = "StudentMultiTool Home";
+            // Change console text color
+            Console.ForegroundColor = ConsoleColor.Green;
+            // Change terminal height
+            Console.WindowHeight = 40;
 
+            // Create UiPrint Object
+            UIPrint ui = new UIPrint();
+
+            // Create bool object for menu loop
+            bool menuLoop = true;
+            // Create loop for menu
+            while (menuLoop is true)
+            {
+                ui.SystemAccountMenu();
+                // Get user choice
+                int menuChoice = Convert.ToInt32(Console.ReadLine());
+                // Complete the appropriate action
+                switch (menuChoice)
+                {
+                    // Break right away
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        menuLoop = false;
+                        break;
+                    // Logout - go back to login 
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+
+                        LoginController login = new LoginController();
+                        login.Authenticate();
+                        menuLoop = false;
+                        break;
+                }
+            }
         }
 
 
