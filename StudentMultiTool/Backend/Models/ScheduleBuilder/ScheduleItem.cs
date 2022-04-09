4f10df08ca1d075483b161595dda2f200b24e9b8
aloafofbrad <aloafofbrad2@gmail.com>
2022-04-08 22:52:20 -0700

    Changed instances of String to instances of string

--- StudentMultiTool/Backend/Models/ScheduleBuilder/ScheduleItem.cs
+++ StudentMultiTool/Backend/Models/ScheduleBuilder/ScheduleItem.cs
@@ -6,7 +6,7 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
     public class ScheduleItem
     {
         // Options for all ScheduleItems. These are maximum
-        // String lengths, intended to preserve storage space.
+        // string lengths, intended to preserve storage space.
         // See ScheduleItem.shorten() for usage.
         public static int maxContactLength { get; } = 64;
         public static int maxLocationLength { get; } = 64;
@@ -21,8 +21,8 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
         // A contact for the ScheduleItem. This can represent a
         // college professor/instructor, a manager or supervisor,
         // or anyone else associated with the ScheduleItem.
-        private String _Contact = String.Empty;
-        public String Contact
+        private string _Contact = string.Empty;
+        public string Contact
         {
             get
             {
@@ -52,8 +52,8 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
         public TimeOnly EndTime { get; set; } = new TimeOnly();
 
         // The physical location of the ScheduleItem.
-        private String _Location = String.Empty;
-        public String Location
+        private string _Location = string.Empty;
+        public string Location
         {
             get
             {
@@ -66,8 +66,8 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
         }
 
         // Custom notes that the user can set for each ScheduleItem
-        private String _Notes = String.Empty;
-        public String Notes
+        private string _Notes = string.Empty;
+        public string Notes
         {
             get
             {
@@ -81,8 +81,8 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
 
         // A user-assigned title/label for the ScheduleItem. Intended to
         // be a class name and/or number, such as "Calculus" or "MATH 123".
-        private String _Title = String.Empty;
-        public String Title
+        private string _Title = string.Empty;
+        public string Title
         {
             get
             {
@@ -136,7 +136,7 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
 
         // Returns a shortened version of the string value, equal in length to the length argument.
         // Intended for use mainly in ScheduleItem.
-        public String shorten(String value, int length)
+        public string shorten(string value, int length)
         {
             if (value.Length <= length)
             {
@@ -149,7 +149,7 @@ namespace StudentMultiTool.Backend.Models.ScheduleBuilder
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return String.Empty;
+                return string.Empty;
             }
         }
     }
