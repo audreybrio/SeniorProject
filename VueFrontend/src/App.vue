08f7b78da51d7695a969e5e1831a05214454bd41
Albert Toscano <57019251+tj5775@users.noreply.github.com>
2022-04-10 19:38:41 -0700

    Documentation Done

---
+++ VueFrontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>test</h1>
+    <RegistrationForm />
+</template>
+
+<script>
+    import RegistrationForm from './components/Registration/RegistrationForm.vue'
+export default {
+  name: 'App',
+        components: {
+            RegistrationForm
+  }
+}
+</script>
+
+<style>
+</style>
