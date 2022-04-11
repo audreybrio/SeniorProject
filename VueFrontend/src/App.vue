192dd83d70097763497133fd45239261008597c3
Albert Toscano <57019251+tj5775@users.noreply.github.com>
2022-04-10 20:56:35 -0700

    Fixed Registration Done

--- VueFrontend/src/App.vue
+++ VueFrontend/src/App.vue
@@ -1,27 +1,15 @@
 <template>
     <h1>test</h1>
-<<<<<<< HEAD
     <RegistrationForm />
 </template>
 
 <script>
     import RegistrationForm from './components/Registration/RegistrationForm.vue'
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
         components: {
             RegistrationForm
-=======
-</template>
+        }
+    }
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
->>>>>>> main
-  }
-}
 </script>
-
-<style>
-</style>
