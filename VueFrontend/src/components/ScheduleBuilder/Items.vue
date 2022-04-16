9e22ea87506383d7fe4cf78d4dce23cf56929f3f
aloafofbrad <aloafofbrad2@gmail.com>
2022-04-15 21:41:41 -0700

    Added item-deleted emit & editable property, cleaned up old comments

--- VueFrontend/src/components/ScheduleBuilder/Items.vue
+++ VueFrontend/src/components/ScheduleBuilder/Items.vue
@@ -1,7 +1,8 @@
 <template>
-    <div v-if="editing" id="editdiv">
+    <div v-if="editing && editable" id="editdiv">
         <UpdateItemForms
                    @item-updated="updateItem"
+                   @item-deleted="deleteItem"
                    @cancel="cancel"
                    :nextId="item.id"
                    :item="item"
@@ -9,74 +10,13 @@
                    :submitText="updateButtonText"
                    >
         </UpdateItemForms>
-        <!--<table>
-            <thead>
-                <tr>
-                    <td>
-                        <button @click="saveEdit()" type="button">Save</button>
-                        <button @click="cancelEdit()" type="reset">Cancel</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for start time -->
-                        <!--<input v-model="tempItem.startHour">
-                        <input v-model="tempItem.startMinute">
-                    </td>
-                </tr>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for end time -->
-                        <!--<input v-model="tempItem.endHour">
-                        <input v-model="tempItem.endMinute">
-                    </td>
-                </tr>-->
-                <!-- TODO: form for days -->
-                <!--<tr>
-                    <td v-for="(day, index) in item.days" :key="day">
-                        <input v-model="tempItem.days[index]" type="checkbox" v-if="day" checked>
-                        <input v-model="tempItem.days[index]" type="checkbox" v-else>
-                    </td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for title -->
-                        <!--<label for="titleEditField">Title</label>
-                        <input v-model="tempItem.title" id="titleEditField">
-                    </td>
-                </tr>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for contact -->
-                        <!--<label for="contactEditField">Contact</label>
-                        <input v-model="tempItem.contact" id="contactEditField">
-                    </td>
-                </tr>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for location -->
-                        <!--<label for="locationEditField">Location</label>
-                        <input v-model="tempItem.location" id="locationEditField">
-                    </td>
-                </tr>
-                <tr>
-                    <td>-->
-                        <!-- TODO: form for notes -->
-                        <!--<label for="notesEditField">Notes</label>
-                        <textarea v-model="tempItem.notes" id="notesEditField" />
-                    </td>
-                </tr>
-            </tbody>
-        </table>-->
     </div>
     <div v-else id="noeditdiv">
         <table>
             <thead>
                 <tr>
                     <td>
-                        <button @click="edit()" type="button">Edit</button>
+                        <button v-if="editable" @click="edit()" type="button">Edit</button>
                     </td>
                 </tr>
                 <tr>
@@ -116,6 +56,7 @@
         props: {
             item: Object,
             Time: Object,
+            editable: Boolean
         },
         components: {
             UpdateItemForms,
@@ -149,37 +90,19 @@
                     endHour: this.item.endHour,
                     endMinute: this.item.endMinute
                 };
-                //this.item.editing = true;
             },
-            //saveEdit(){
-            //    // Update the vaules
-            //    this.item = {
-            //        id: this.item.id,
-            //        title: this.tempItem.title,
-            //        contact: this.tempItem.contact,
-            //        location: this.tempItem.location,
-            //        notes: this.tempItem.notes,
-            //        days: this.tempItem.days,
-            //        startHour: this.tempItem.startHour,
-            //        startMinute: this.tempItem.startMinute,
-            //        endHour: this.tempItem.endHour,
-            //        endMinute: this.tempItem.endMinute
-            //    };
-            //    //this.item.editing = false;
-            //    this.editing = false;
-            //},
             cancelEdit(){
-                //this.item.editing = false;
                 this.editing = false;
-                // Don't update this.item's values
             },
             updateItem(updatedItem) {
-                // TODO: emit to days
-                //this.item = newItem;
+                // emit to parent component
                 this.$emit("item-updated", updatedItem);
             },
             cancel() {
                 this.editing = false;
+            },
+            deleteItem(deleteableItem) {
+                this.$emit("item-deleted", deleteableItem);
             }
         }
     }
