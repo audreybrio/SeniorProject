0acb86bff7cc11eabd0195fc0ae51512f8f2cbd0
aloafofbrad <aloafofbrad2@gmail.com>
2022-04-22 00:29:29 -0700

    Added support for "page not found" view and did minor organizations

--- VueFrontend/src/router/index.js
+++ VueFrontend/src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
+// Import your views here
+// Core Features
 import RegistrationForm from '../components/Registration/RegistrationForm.vue'
 import EmailVue from '../components/Authentication/EmailVue.vue'
 import LoginVue from '../components/Authentication/LoginVue.vue'
 import HomePage from '../Views/HomePage.vue'
-import ScheduleBuilder from '../components/ScheduleBuilder/ScheduleBuilder.vue'
-import ScheduleSelection from '../components/ScheduleBuilder/ScheduleSelection.vue'
+
+// Application features
+// StudentDiscounts
 import StudentDiscounts from '../components/StudentDiscounts/StudentDiscounts.vue'
+
+// Defining the routes
 const routes = [
+    // Core Features
     {
         path: '/',
         name: 'EmailVue',
@@ -29,16 +36,32 @@ const routes = [
         name: 'RegistrationForm',
         component: RegistrationForm
     },
+
+    // ScheduleBuilder & ScheduleComparison
+    // ScheduleBuilder
     {
-        path: '/schedulebuilder/:user/:scheduleId',
+        path: '/schedule/builder/:user/:scheduleId',
         name: 'ScheduleBuilder',
-        component: ScheduleBuilder
+        component: () => import('../components/ScheduleBuilder/ScheduleBuilder.vue')
     },
     {
-        path: '/scheduleselection/:user',
-        name: 'ScheduleSelection',
-        component: ScheduleSelection
+        path: '/schedule/builder/select/:user',
+        name: 'SelectForBuilder',
+        component: () => import('../components/ScheduleBuilder/SelectForBuilder.vue')
     },
+    // ScheduleComparison
+    {
+        path: '/schedule/comparison/:user/:selection',
+        name: 'ScheduleComparison',
+        component: () => import('../components/ScheduleComparison/ScheduleComparison.vue')
+    },
+    {
+        path: '/schedule/comparison/select/:user',
+        name: 'SelectForComparison',
+        component: () => import('../components/ScheduleComparison/SelectForComparison.vue')
+    },
+
+    // BookSelling
     {
         path: '/bookSelling',
         name: 'bookSelling',
@@ -54,17 +77,32 @@ const routes = [
         name: 'bookPost',
         component: () => import('../Views/BookSelling/PostBook.vue')
     },
+
+    // StudentDiscounts
     {
         path: '/studentDiscounts',
         name: 'studentDiscounts',
         component: StudentDiscounts
     },
-    // add more here 
+    // add more here
+
+    // Not found; don't move this one or place anything after it
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        component: () => import('../Views/NotFound')
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+// Resolve not found
+router.resolve({
+    name: 'not-found',
+    params: { pathMatch: ['not', 'found'] },
+}).href
 
 export default router
