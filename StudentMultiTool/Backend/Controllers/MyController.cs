134e4f078d722fe53fb6ea4d9b3608d05711744a
aloafofbrad <aloafofbrad2@gmail.com>
2022-04-22 00:27:21 -0700

    Initial commit

---
+++ StudentMultiTool/Backend/Controllers/MyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentMultiTool.Backend.Controllers
+{
+    // Contains debug actions
+    [ApiController]
+    [Route("mycontroller")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult UtcNow()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+
+        [HttpGet]
+        public IActionResult ControllersWorking()
+        {
+            return Ok("Controllers are working");
+        }
+
+        [HttpGet("[action]/{text}")]
+        public IActionResult Echo(string text)
+        {
+            return Ok(text);
+        }
+    }
+}
---
+++ VueFrontend/src/components/ScheduleComparison/SelectForComparison.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="list">
+        <div>
+            <h2>
+                Schedule Comparison
+            </h2>
+        </div>
+        <div v-if="loading" class="loading">
+            Loading...
+        </div>
+
+        <div v-if="schedules.length > 0" class="content">
+            <button v-if="selectionLengthIsInRange" @click="onScheduleComparison()">
+                Compare!
+            </button>
+            <h4 v-else>Select 2 to 5 schedules for comparison.</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Compare</th>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>created</th>
+                        <th>modified</th>
+                        <th>Owner</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="schedule in schedules" :key="schedule.id">
+                        <!--TODO: implement Schedule component-->
+                        <!--<Schedule :schedule="schedule"/>-->
+                        <td><input type="checkbox" @click="toggleSelection(schedule.id)" /></td>
+                        <td>{{ schedule.id }}</td>
+                        <td>{{ schedule.title }}</td>
+                        <td>{{ schedule.created }}</td>
+                        <td>{{ schedule.modified }}</td>
+                        <td>{{ schedule.owner }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>
+            <p>Create a schedule or two to get started :)</p>
+            <button @click="onScheduleBuilder">Go to schedule builder</button>
+        </div>
+    </div>
+    <router-view />
+</template>
+
+<script lang="js">
+    import * as $ from 'jquery'
+    import router from '../../router'
+    const baseURL = "https://localhost:5002"
+    import URLs from '../../variables'
+    export default ({
+        data() {
+            return {
+                loading: false,
+                schedules: [],
+                selection: [],
+                newScheduleTitle: "",
+
+                // get user id or other identifier from the router to plug into getList()
+                user: this.$route.params.user
+            };
+        },
+        computed: {
+            selectionLengthIsInRange() {
+                return this.selection.length >= 2 && this.selection.length <= 5;
+            }
+        },
+        created() {
+            this.selection = [];
+            console.log(URLs);
+            console.log("this.user: " + this.user);
+            this.getList();
+        },
+        watch: {
+            // call again the method if the route changes
+            '$route': 'getList'
+        },
+        methods: {
+            // Toggle whether or not the schedule was checked. If it was, add it to the 
+            toggleSelection(scheduleID) {
+                let index = this.selection.find(id => id === scheduleID);
+                if (index) {
+                    this.selection = this.selection.filter(id => id !== scheduleID);
+                }
+                else {
+                    this.selection.push(scheduleID);
+                }
+                console.log(this.selection);
+            },
+            getList() {
+                this.loading = true;
+                let requestName = "ScheduleComparison- get list of schedules";
+                console.log(requestName);
+                $.ajax({
+                    // set the HTTP request URL
+                    url: `${baseURL}/api/schedule/getlist/${this.user}`,
+
+                    // set the context object to the vue component
+                    // this line tells vue to update its components
+                    // when the success or error objects complete!
+                    // if it's not set, the components don't update!
+                    context: this,
+
+                    // HTTP method
+                    method: 'GET',
+
+                    // On a successful AJAX request:
+                    success: function (data) {
+                        this.schedules = data;
+                        this.loading = false;
+                        // TODO: delete console.logs
+                        console.log("this.list:")
+                        console.log(this.list)
+                        console.log("this.loading:")
+                        console.log(this.loading)
+                        // log that we've completed
+                        console.log(requestName + "- Success")
+                        return true;
+                    },
+
+                    // On an unsuccessful AJAX request:
+                    error: function (error) {
+                        // log the error
+                        console.log(requestName + "- Error");
+                        console.log(error);
+                        this.schedules = [];
+                        this.loading = false;
+                        return false;
+                    }
+                });
+            },
+            onScheduleBuilder() {
+                router.push({
+                    name: 'ScheduleSelection',
+                    params: { user: this.user }
+                });
+            },
+            onScheduleComparison() {
+                if (!this.selectionLengthIsInRange) {
+                    alert("Something went wrong.\nSelect 2 to 5 schedules for comparison.");
+                }
+                else {
+                    router.push({
+                        name: 'ScheduleComparison',
+                        params: { user: this.user, selection: this.selection }
+                    });
+                }
+            },
+        }
+    });
+</script>
+
+<style>
+    table {
+        margin: auto;
+    }
+    td {
+        border-width: 1px;
+        border-style: solid;
+        border-color: black;
+    }
+</style>
