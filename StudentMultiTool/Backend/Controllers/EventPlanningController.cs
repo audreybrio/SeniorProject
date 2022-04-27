89cd77cc381cc6ec923a83dd5678ee1863104a13
SzeManTang <72945488+SzeManTang@users.noreply.github.com>
2022-04-27 10:27:31 -0700

    backend

---
+++ StudentMultiTool/Backend/Controllers/EventPlanningController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentMultiTool.Backend.Services.EventPlanning;
+using StudentMultiTool.Backend.Services.UserManagement;
+
+namespace StudentMultiTool.Backend.Controllers
+{
+    [Route("api/" + "eventplanning")]
+    [ApiController]
+    public class EventPlanningController : ControllerBase
+    {
+        private readonly ILogger<EventPlanning> _logger;
+        public EventPlanningController(ILogger<EventPlanning> logger)
+        {
+            _logger = logger;
+        }
+
+        // post event for Establishment
+        [HttpPost]
+        [HttpPost("postEstablishment/{eventtitle}/{eventtime}/{date}/{location}/{description}")]
+        public IActionResult postEstablishment(string eventtitle, string eventtime, string date, string location, string description)
+        {
+            EventPlanningUi post = new EventPlanningUi();
+            if(post.postEstablishment(eventtitle, eventtime, date, location, description))
+            {
+                return Ok("Success");
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+       
+
+        // get posts for Establishments
+        [HttpGet("getEventPlanning")]
+        public IEnumerable<EventPlanning> getDiscountsWe()
+        {
+
+            EventPlanningUi post = new EventPlanningUi();
+            return post.getEventPlanning();
+
+        }
+
+        // get post details for establishment
+        [HttpGet("getDetails /{id}")]
+        public IEnumerable<EventPlanning> getDetailsEstablishment(string id)
+        {
+
+            EventPlanningUi post = new EventPlanningUi();
+            return post.getDetails(id);
+
+        }
+    }
+}
