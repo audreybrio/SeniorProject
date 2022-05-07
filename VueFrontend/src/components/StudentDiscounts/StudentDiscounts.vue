03a620c746796032f7cd1496bcc1f13735b943ee
Albert Toscano <57019251+tj5775@users.noreply.github.com>
2022-05-06 23:42:55 -0700

    changed student discounts

--- VueFrontend/src/components/StudentDiscounts/StudentDiscounts.vue
+++ VueFrontend/src/components/StudentDiscounts/StudentDiscounts.vue
@@ -1,4 +1,4 @@
-<!--<template>
+<template>
     <div class="page">
         <div>
             <h2 class="row1">Student Discounts</h2>
@@ -32,26 +32,26 @@
             PostDiscounts,
             SearchDiscounts
         },
-        data(){
-          return{
-            isSearch: true,
-            isPost: false
-          }
+        data() {
+            return {
+                isSearch: true,
+                isPost: false
+            }
 
         },
         methods: {
-          searchSwitch(){
-              if (!this.isSearch) {
-              this.isSearch = true
-              this.isPost = false
+            searchSwitch() {
+                if (!this.isSearch) {
+                    this.isSearch = true
+                    this.isPost = false
+                }
+            },
+            postSwitch() {
+                if (!this.isPost) {
+                    this.isSearch = false
+                    this.isPost = true
+                }
             }
-          },
-          postSwitch(){
-            if (!this.isPost){
-              this.isSearch = false
-              this.isPost = true
-            }
-          }
 
         },
     }</script>
@@ -62,9 +62,10 @@
         margin-right: 20px;
         float: right;
     }
+
     .page {
         margin: auto;
-        padding: auto;
+        /*padding: auto;*/
     }
 
     .header {
@@ -92,32 +93,4 @@
         border: 5mm;
         padding-bottom: 0.5em;
     }
-
-    /*#topButtons{
-        padding: 30px;
-    }
-    #topButtons a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-    #topButtons a.router-link-exact-active{
-        color: white;
-        background-color: crimson;
-        padding: 10px;
-        border-radius: 5px;
-    }
-    .action {
-        align-content: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: 10mm;
-    }
-
-    .ui.button,
-    .dot.circle.icon {
-        background-color: silver;
-        color: white;
-        width: 20mm;
-    }*/
-</style>-->
+</style>
