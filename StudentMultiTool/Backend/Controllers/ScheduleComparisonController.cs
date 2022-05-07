e3c7313c917bee96f97999b0c9d13c7b118aa658
aloafofbrad <aloafofbrad2@gmail.com>
2022-05-06 21:48:44 -0700

    Moved logic out of controller

--- StudentMultiTool/Backend/Controllers/ScheduleComparisonController.cs
+++ StudentMultiTool/Backend/Controllers/ScheduleComparisonController.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StudentMultiTool.Backend.Models.ScheduleBuilder;
-using StudentMultiTool.Backend.Services.ScheduleBuilder;
 using StudentMultiTool.Backend.Services.ScheduleComparison;
-using System.Data;
-using System.Data.SqlClient;
-using System.Text.Json;
-using System.Text.Json.Nodes;
 
 namespace StudentMultiTool.Backend.Controllers
 {
@@ -19,85 +14,8 @@ namespace StudentMultiTool.Backend.Controllers
         [HttpGet("getcomparison")]
         public IEnumerable<ScheduleItemDTO> GetComparison([FromQuery] string user, [FromQuery] List<string> scheduleIds)
         {
-            List<ScheduleItemDTO> items = new List<ScheduleItemDTO>();
-            List<int> ids = new List<int>();
-
-            if (scheduleIds != null && scheduleIds.Count > 0)
-            {
-                // Try to deserialize the numbers from the query string.
-                List<string> rawIds;
-                try
-                {
-                    rawIds = JsonSerializer.Deserialize<List<string>>(scheduleIds[0]);
-                    foreach (string id in rawIds)
-                    {
-                        // Try to add the parsed ids into the list to be compared.
-                        try
-                        {
-                            ids.Add(int.Parse(id));
-                        }
-
-                        // For all three kinds of exceptions that int.Parse() throws,
-                        // we don't want to try to compare anything.
-                        catch (Exception ex)
-                        {
-                            Console.Error.WriteLine(ex.GetType().FullName);
-                            Console.Error.WriteLine(ex.Message);
-                            return items;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return items;
-                }
-            }
-
-            // Check that the user has permission to edit the schedule
-            SchedulePermissionValidator validator = new SchedulePermissionValidator();
-            ScheduleListBuilder listBuilder = new ScheduleListBuilder();
-            string? hash = listBuilder.GetUserHash(user);
-            bool isCollaborator = false;
-
-            // If the user's hash is null, then they probably aren't logged in
-            if (!string.IsNullOrEmpty(hash))
-            {
-                isCollaborator = validator.ValidateAll(hash, ids);
-            }
-
-            // If the user is a collaborator on the schedule, continue
-            if (isCollaborator && (ids.Count >= 2 && ids.Count <= 5))
-            {
-                // Get each schedule's items
-                ScheduleManager manager = new ScheduleManager();
-                List<Schedule> schedules = new List<Schedule>();
-                for (int i = 0; i < ids.Count; i++)
-                {
-                    Schedule? current = manager.SelectScheduleWithItems(ids[i]);
-                    if (current != null)
-                    {
-                        schedules.Add(current);
-                    }
-                }
-
-                // Compare the schedules
-                ScheduleComparator comparator = new ScheduleComparator();
-                Schedule result = comparator.VisualRepresentation(schedules);
-
-                // Prep the items for data transfer
-                foreach (ScheduleItem si in result.Items)
-                {
-                    Console.WriteLine(si.Title);
-                    ScheduleItemDTO temp = new ScheduleItemDTO(si);
-                    temp.ScheduleId = -1;
-                    items.Add(temp);
-                }
-                
-                // Return the items
-                return items;
-            }
-            // If something went wrong or the user wasn't a collaborator, return an empty list
-            return Enumerable.Empty<ScheduleItemDTO>();
+            ComparisonManager manager = new ComparisonManager();
+            return manager.GetComparison(user, scheduleIds);
         }
 
         // Return a comparison of schedules given the user and the IDs of the schedules to compare.
@@ -105,79 +23,8 @@ namespace StudentMultiTool.Backend.Controllers
         [HttpGet("getminutes")]
         public int GetMinutes([FromQuery] string user, [FromQuery] List<string> scheduleIds)
         {
-            int result = 0;
-            List<int> ids = new List<int>();
-
-            if (scheduleIds != null && scheduleIds.Count > 0)
-            {
-                List<string> rawIds;
-                // Try to deserialize the ids from the query string.
-                try
-                {
-                    rawIds = JsonSerializer.Deserialize<List<string>>(scheduleIds[0]);
-                    foreach (string id in rawIds)
-                    {
-                        // Try to add the parsed ids into the list to be compared.
-                        try
-                        {
-                            ids.Add(int.Parse(id));
-                        }
-
-                        // For all three kinds of exceptions that int.Parse() throws,
-                        // we don't want to try to compare anything.
-                        catch (Exception ex)
-                        {
-                            Console.Error.WriteLine(ex.GetType().FullName);
-                            Console.Error.WriteLine(ex.Message);
-                            return -1;
-                        }
-                    }
-                }
-                // If the query string couldn't be deserialized, we can't really do anything.
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex.GetType().FullName);
-                    Console.Error.WriteLine(ex.Message);
-                    return -1;
-                }
-            }
-
-            // Check that the user has permission to edit the schedule
-            SchedulePermissionValidator validator = new SchedulePermissionValidator();
-            ScheduleListBuilder listBuilder = new ScheduleListBuilder();
-            string? hash = listBuilder.GetUserHash(user);
-            bool isCollaborator = false;
-
-            // If the user's hash is null, then they probably aren't logged in
-            if (!string.IsNullOrEmpty(hash))
-            {
-                isCollaborator = validator.ValidateAll(hash, ids);
-            }
-
-            // If the user is a collaborator on the schedule, continue
-            if (isCollaborator && (ids.Count >= 2 && ids.Count <= 5))
-            {
-                // Get each schedule's items
-                ScheduleManager manager = new ScheduleManager();
-                List<Schedule> schedules = new List<Schedule>();
-                for (int i = 0; i < ids.Count; i++)
-                {
-                    Schedule? current = manager.SelectScheduleWithItems(ids[i]);
-                    if (current != null)
-                    {
-                        schedules.Add(current);
-                    }
-                }
-
-                // Compare the schedules
-                ScheduleComparator comparator = new ScheduleComparator();
-                result = comparator.TotalFreeMinutes(schedules);
-
-                // Return the total number of shared free minutes
-                return result;
-            }
-            // If something went wrong or the user wasn't a collaborator, return an empty list
-            return -1;
+            ComparisonManager manager = new ComparisonManager();
+            return manager.GetMinutes(user, scheduleIds);
         }
     }
 }
